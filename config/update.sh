ba3eb16078fb86dd6a7f653921796075814d270f
Oleg Abrosimov <olegabr@mail.ru>
2014-06-13 01:13:39 +0700

    Update update.sh
    Fixed the http://examples.qcu.be/assets/_core/php/examples/other_controls/file_asset.php
    It needs the folder to be writable.

--- config/update.sh
+++ config/update.sh
@@ -4,3 +4,4 @@
 ./installPlugins
 chown www-data:www-data ../htdocs/includes/error_log/
 chown www-data:www-data ../htdocs/assets/_core/php/examples/other_controls/cache/
+chown www-data:www-data ../htdocs/assets/_core/images/cache/
