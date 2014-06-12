7dfe1b64fce9bd82d3085fd3911b00ab73b3bfb3
Oleg Abrosimov <olegabr@mail.ru>
2014-06-13 01:14:43 +0700

    Update update.sh
    Fixes the http://examples.qcu.be/assets/_core/php/examples/image_browser/test_image_browser.php

--- config/update.sh
+++ config/update.sh
@@ -5,3 +5,4 @@
 chown www-data:www-data ../htdocs/includes/error_log/
 chown www-data:www-data ../htdocs/assets/_core/php/examples/other_controls/cache/
 chown www-data:www-data ../htdocs/assets/_core/images/cache/
+chown www-data:www-data ../htdocs/assets/_core/php/examples/image_browser/cache/
